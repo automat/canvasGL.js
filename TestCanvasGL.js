b4ad430363ba407b760a8feffe6beb08c5f4bd37
automat <olafyoorsen@hotmail.com>
2012-12-28 20:45:00 +0100

    Changed to DYNAMIC_DRAW, impl. matrices, bezier curvers....

--- TestCanvasGL.js
+++ TestCanvasGL.js
@@ -30,13 +30,31 @@ TestCanvasGL.prototype.draw = function()
 
     this.t+=0.05;
 
+    var t = this.t;
+
     var c = this.c;
 
     c.background(0);
     c.fillf(1);
     c.rect(0,0, 200,100);
-    c.fill(255,0,0);
+    c.fill(150,0,0);
     c.rect(0,100,100,100);
+
+    c.fill(200,0,0);
+
+    //0----2----4
+    //|  / |  / |
+    //| /  | /  |
+    //1----3----5
+
+    c.stroke(255);
+    var i,j;
+    var verticesA = [0,200,0,300,100,200,100,300,0,200,0,300,100,200,100,300];
+    c.triangleMesh([0,200,0,300,100,200,100,300]);
+
+
+
+
     c.fill(120);
     c.circle(150,150,50);
     c.fill(60);
@@ -47,7 +65,7 @@ TestCanvasGL.prototype.draw = function()
     c.lines([300,100,400,200,500,100]);
 
     c.fill(255);
-    var i,j;
+
     i = -1;
     var a = 30;
     var s = 300 / (a-1);
@@ -63,11 +81,11 @@ TestCanvasGL.prototype.draw = function()
     c.stroke(255,0,0);
     c.fill(255);
 
-    var verticesA = [500,100,550,100,550,150,600,100];
-    var indices  = [0,1,2,1,3,2];
+    verticesA = [500,100,550,100,550,150,600,100];
+    indices  = [0,1,2,1,3,2];
 
 
-    c.triangleMesh(verticesA,indices);
+    //c.triangleMesh(verticesA,indices);
 
     verticesA = [];
 
@@ -84,6 +102,7 @@ TestCanvasGL.prototype.draw = function()
     c.fill(255);
     c.points(verticesA);
 
+    indices = [];
     var verticesB = [];
 
     a = 100;
@@ -92,26 +111,69 @@ TestCanvasGL.prototype.draw = function()
     while(++i < a)
     {
         verticesA.push(200+i*s,350+Math.sin(i/(a+1)*5-this.t*2)*50);
-        verticesB.push(200+i*s,350+Math.sin(i/(a+1)*5-this.t*2)*50);
-        verticesB.push(200+i*s,400);
+
+        verticesB.push(200+i*s,350+Math.sin(i/(a+1)*5-this.t*2)*50,200+i*s,400);
+
     }
 
+
+
+
+
+    c.fill(100,0,0);
+    c.triangleMesh(verticesB);
     c.fill(255);
-    c.lines(verticesB);
     c.points(verticesA);
+
 
     verticesA = [];
 
-    a = 400;
+    a = 5 + 100 * Math.abs(Math.sin(t*0.05));
     s = Math.PI / (a-1);
     i = -1;
+
+    var R, r,k;
+
+
+
+    r = 11.5;
+    k = 2+Math.floor(10*Math.abs(Math.sin(t*0.5)));
+    R = k*r;
+
+
+    var e, f, g,h;
+    var x,y;
+
     while(++i < a)
     {
-        verticesA.push(350+150*Math.sin(i*0.25+this.t*0.25)*Math.cos(i*2*s+this.t*0.25),550+150*Math.sin(i*0.25+this.t*0.25)*Math.sin(i*2*s+this.t*0.25));
+
+
+        x = 350+(R + r)*Math.cos(s*i*2+t)-(r*Math.cos(((R+r)/r)*(s*i*2+t)));
+        y = 550+(R + r)*Math.sin(s*i*2+t)-(r*Math.sin(((R+r)/r)*(s*i*2+t)));
+
+        verticesA.push(x,y);
     }
 
     c.lines(verticesA);
     c.points(verticesA);
+
+
+    c.stroke(255);
+
+    var ox = 510,oy = 410, sxy = 2;
+    var anchor0x = ox+150,anchor0y = oy,
+        contrl0x = ox,contrl0y = oy,
+        anchor1x = ox+150,anchor1y = oy+200+100*Math.abs(Math.cos(t)),
+        contrl1x = ox,contrl1y = oy+200+100*Math.abs(Math.sin(t));
+
+
+
+    c.setBezierDetail(140);
+    c.bezier(anchor0x,anchor0y,contrl0x,contrl0y,anchor1x,anchor1y,contrl1x,contrl1y);
+
+    c.stroke(255,102,0);
+    c.line(anchor0x,anchor0y,contrl0x,contrl0y);
+    c.line(anchor1x,anchor1y,contrl1x,contrl1y);
 
 
 
--- js/canvasGL.js
+++ js/canvasGL.js
@@ -13,7 +13,9 @@ CanvasGLOptions.RESOLUTION_CIRCLE = 20;
 _InternalCanvasGLOptions = {};
 _InternalCanvasGLOptions.DEFAULT_WIDTH  = 300;
 _InternalCanvasGLOptions.DEFAULT_HEIGHT = 300;
+_InternalCanvasGLOptions.BEZIER_DETAIL  = 20;
 
+_InternalCanvasGLConstants = {};
 
 function CanvasGL(parentDomElementId)
 {
@@ -33,12 +35,13 @@ function CanvasGL(parentDomElementId)
     {
         try
         {
-            this.gl = this._glCanvas.getContext(names[i]);
+            this.gl = this._glCanvas.getContext(names[i],{ antialias: true});
         } catch (e){if(CanvasGLOptions.doLog)console.log("WebGL context could not be initialized");}
         if(this.gl){if(CanvasGLOptions.doLog)console.log("WebGL context initialized: "+names[i]);break;}
     }
 
-    this._vertexShader   = this._loadShader("attribute vec2 a_position; uniform vec2 u_resolution;void main() {vec2 zeroToOne = a_position / u_resolution;vec2 zeroToTwo = zeroToOne * 2.0;vec2 clipSpace = zeroToTwo - 1.0;gl_Position = vec4(clipSpace * vec2(1, -1), 0, 1);}",this.gl.VERTEX_SHADER);
+   // this._vertexShader   = this._loadShader("attribute vec2 a_position; uniform vec2 u_resolution;void main() {vec2 zeroToOne = a_position / u_resolution;vec2 zeroToTwo = zeroToOne * 2.0;vec2 clipSpace = zeroToTwo - 1.0;gl_Position = vec4(clipSpace * vec2(1, -1), 0, 1);}",this.gl.VERTEX_SHADER);
+    this._vertexShader   = this._loadShader("uniform mat4 uMVMatrix; uniform mat4 uPMatrix;attribute vec2 a_position; uniform vec2 u_resolution;void main() {vec2 zeroToOne = a_position / u_resolution;vec2 zeroToTwo = zeroToOne * 2.0;vec2 clipSpace = zeroToTwo - 1.0;gl_Position = uPMatrix * uMVMatrix * vec4(clipSpace * vec2(1, -1), 0, 1);}",this.gl.VERTEX_SHADER);
     this._fragmentShader = this._loadShader("precision mediump float;uniform vec4 u_color;void main(){gl_FragColor = u_color;}",this.gl.FRAGMENT_SHADER);
     this._program        = this._loadProgram();
 
@@ -55,7 +58,17 @@ function CanvasGL(parentDomElementId)
 
     this._vertexPostionBuffer = gl.createBuffer();
     this._vertexIndexBuffer   = gl.createBuffer();
+    this._vertexTexCoordBuffer= gl.createBuffer();
     this._vertexColorBuffer   = gl.createBuffer();
+
+    this._pUniform  = gl.getUniformLocation(this._program,"uPMatrix");
+    this._mvUniform = gl.getUniformLocation(this._program,"uMVMatrix");
+    this._mvMatrix = _makeMat44();
+
+    gl.uniformMatrix4fv(this._pUniform,false, new Float32Array(_makeMat44()));
+    gl.uniformMatrix4fv(this._mvUniform,false,new Float32Array(this._mvMatrix));
+
+
 
     gl.bindBuffer(gl.ARRAY_BUFFER,this._vertexPostionBuffer);
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER,this._vertexIndexBuffer);
@@ -65,7 +78,6 @@ function CanvasGL(parentDomElementId)
     gl.vertexAttribPointer(this._positionLocation,2,gl.FLOAT,false,0,0);
 
     this._matrixStack = [];
-
 
     this._pixelPerfect = false;
 
@@ -78,19 +90,16 @@ function CanvasGL(parentDomElementId)
 
 }
 
-CanvasGL.prototype.triangleMesh = function(vertices,indices)
+CanvasGL.prototype._loadIdentity = function()
 {
-    var gl = this.gl;
-    gl.bufferData(gl.ARRAY_BUFFER,new Float32Array(vertices),gl.STATIC_DRAW);
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER,new Uint16Array(indices),gl.STATIC_DRAW);
-
-    if(this._fill)
-    {
-        this._applyFill();
-        gl.drawElements(gl.TRIANGLES,indices.length,gl.UNSIGNED_SHORT,0);
-    }
+    _mat44Identity(this._mvMatrix);
 };
 
+CanvasGL.prototype._multMatrix = function()
+{
+
+
+};
 
 CanvasGL.prototype.push = function()
 {
@@ -112,10 +121,40 @@ CanvasGL.prototype.scale = function(x,y)
 
 };
 
-CanvasGL.prototype.setPixelPerfect = function(bool)
+CanvasGL.prototype.rotate = function(x,y)
 {
-    this._pixelPerfect = bool;
+
 };
+
+CanvasGL.prototype.rotateX = function(r)
+{
+
+};
+
+CanvasGL.prototype.rotateY = function(r)
+{
+
+};
+
+CanvasGL.prototype.triangleMesh = function(vertices,indices)
+{
+    var gl = this.gl;
+    var ind = new Uint16Array(indices || this._indicesLinearCW(vertices));
+
+    gl.bufferData(gl.ARRAY_BUFFER,new Float32Array(vertices),gl.STATIC_DRAW);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER,ind,gl.STATIC_DRAW);
+
+    if(this._fill)
+    {
+        this._applyFill();
+        gl.drawElements(gl.TRIANGLES,ind.length,gl.UNSIGNED_SHORT,0);
+    }
+};
+
+
+
+
+
 
 CanvasGL.prototype.line = function(x0,y0,x1,y1)
 {
@@ -123,7 +162,7 @@ CanvasGL.prototype.line = function(x0,y0,x1,y1)
     var gl  = this.gl;
     var arr =  [x0,y0,x1,y1];
     arr = this._pixelPerfect ? this._flooredArray(arr) : arr;
-    gl.bufferData(gl.ARRAY_BUFFER,new Float32Array(arr),gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER,new Float32Array(arr),gl.DYNAMIC_DRAW);
     this._applyStroke();
     gl.drawArrays(gl.LINES,0,2);
 };
@@ -133,7 +172,7 @@ CanvasGL.prototype.lines = function(vertices)
     if(!this._stroke)return;
     var gl  = this.gl;
     var arr = this._pixelPerfect ? this._flooredArray(vertices) : vertices;
-    gl.bufferData(gl.ARRAY_BUFFER,new Float32Array(arr),gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER,new Float32Array(arr),gl.DYNAMIC_DRAW);
     this._applyStroke();
     gl.drawArrays(gl.LINE_STRIP,0,arr.length*0.5);
 };
@@ -144,7 +183,7 @@ CanvasGL.prototype.point = function(x,y)
     var gl = this.gl;
     var arr =  [x,y];
     arr = this._pixelPerfect ? this._flooredArray(arr) : arr;
-    gl.bufferData(gl.ARRAY_BUFFER,new Float32Array(arr),gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER,new Float32Array(arr),gl.DYNAMIC_DRAW);
     this._applyFill();
     gl.drawArrays(gl.POINTS,0,1);
 };
@@ -154,7 +193,7 @@ CanvasGL.prototype.points = function(vertices)
     if(!this._fill)return;
     var gl  = this.gl;
     var arr = this._pixelPerfect ? this._flooredArray(vertices) : vertices;
-    gl.bufferData(gl.ARRAY_BUFFER,new Float32Array(arr),gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER,new Float32Array(arr),gl.DYNAMIC_DRAW);
     this._applyFill();
     gl.drawArrays(gl.POINTS,0,arr.length*0.5);
 };
@@ -177,7 +216,7 @@ CanvasGL.prototype.ellipse = function(x,y,radiusX,radiusY,resolution)
 
     var arr = this._pixelPerfect ? this._flooredArray(v) : v;
 
-    gl.bufferData(gl.ARRAY_BUFFER,arr,gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER,arr,gl.DYNAMIC_DRAW);
 
     if(this._fill)
     {
@@ -192,14 +231,12 @@ CanvasGL.prototype.circle = function(x,y,radius,resolution)
     this.ellipse(x,y,radius,radius,resolution);
 };
 
-CanvasGL.prototype.rect = function(x,y,width,height)
+CanvasGL.prototype.quad = function(x0,y0,x1,y1,x2,y2,x3,y3)
 {
     var gl = this.gl;
-    var xw = x+width,yh = y+height;
-    var arr = [x,y,xw,y,x,yh,x,yh,xw,y,xw,yh];
-
+    var arr = [x0,y0,x1,y1,x3,y3,x1,y1,x2,y2,x3,y3];
     arr = this._pixelPerfect ? this._flooredArray(arr) : arr;
-    gl.bufferData(gl.ARRAY_BUFFER,new Float32Array([x,y,xw,y,x,yh,x,yh,xw,y,xw,yh]),gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER,new Float32Array(arr),gl.DYNAMIC_DRAW);
 
     if(this._fill)
     {
@@ -207,6 +244,90 @@ CanvasGL.prototype.rect = function(x,y,width,height)
         gl.drawArrays(gl.TRIANGLES,0,6);
     }
 };
+
+CanvasGL.prototype.triangle = function(x0,y0,x1,y1,x2,y2)
+{
+    var gl = this.gl;
+    var arr = [x0,y0,x1,y1,x2,y2];
+    arr = this._pixelPerfect ? this._flooredArray(arr) : arr;
+    gl.bufferData(gl.ARRAY_BUFFER,new Float32Array(arr),gl.DYNAMIC_DRAW);
+
+    if(this._fill)
+    {
+        this._applyFill();
+        gl.drawArrays(gl.TRIANGLES,0,3);
+    }
+};
+
+CanvasGL.prototype.setBezierDetail = function(detail)
+{
+    _InternalCanvasGLOptions.BEZIER_DETAIL = detail;
+};
+
+CanvasGL.prototype.bezier = function(x0,y0,x1,y1,x2,y2,x3,y3)
+{
+    var d = _InternalCanvasGLOptions.BEZIER_DETAIL;
+
+    var i = -1;
+    var t,nt,nt3,nt2,t3,t2;
+
+    var vertices = new Array(d);
+
+    while(++i < d)
+    {
+        t   = i / (d-1);
+        nt  = 1 - t;
+        nt3 = Math.pow(nt,3);
+        nt2 = Math.pow(nt,2);
+        t3  = Math.pow(t,3);
+        t2  = Math.pow(t,2);
+
+        vertices.push(nt3*x0+3*nt2*t*x1+3*nt*t2*x2+t3*x3,
+                      nt3*y0+3*nt2*t*y1+3*nt*t2*y2+t3*y3);
+
+    }
+
+    this.lines(vertices);
+};
+
+CanvasGL.prototype.bezierPoint = function(a,b,c,d,t)
+{
+
+};
+
+CanvasGL.prototype.loadImage = function(path,callback)
+{
+    var image = new Image();
+    image.onload = function(){callback();};
+    image.src = path;
+
+    return image;
+};
+
+CanvasGL.prototype.texture = function(img)
+{
+    var gl = this.gl;
+    var texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, texture.image);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    gl.bindTexture(gl.TEXTURE_2D, null);
+
+};
+
+CanvasGL.prototype.image = function(img,x,y,width,height)
+{
+
+};
+
+CanvasGL.prototype.rect = function(x,y,width,height)
+{
+    var xw = x+width,yh = y+height;
+    this.quad(x,y,xw,y,xw,yh,x,yh);
+};
+
 CanvasGL.prototype.noFill = function()
 {
     this._fill = false;
@@ -275,6 +396,8 @@ CanvasGL.prototype.setSize = function(width,height)
     this._glCanvas.width  = styleWidth;
     this._glCanvas.height = styleHeight;
 
+
+
     this.width = this._size.width;
     this.height = this._size.height;
 
@@ -291,6 +414,40 @@ CanvasGL.prototype._flooredArray = function(array)
     {
         a[i] = Math.floor(a[i]);
     }
+    return a;
+};
+
+CanvasGL.prototype._indexSortedVertices = function(vertices,indices)
+{
+
+
+};
+
+CanvasGL.prototype._indicesLinearCW = function(vertices)
+{
+    var a = new Array((vertices.length/2-2)*3);
+    var i = 0;
+    while(i < a.length)
+    {
+        if(i%2==0){a[i]=i/3;a[i+1]=i/3+2;a[i+2]=i/3+1;}
+        else{a[i]=a[i-2];a[i+1]=a[i-2]+1;a[i+2]=a[i-1];}
+        i+=3;
+    }
+
+    return a;
+};
+
+CanvasGL.prototype._indicesLinearCCW = function(vertices)
+{
+    var a = new Array((vertices.length/2-2)*3);
+    var i = 0;
+    while(i < a.length)
+    {
+        if(i%2==0){a[i]=i/3;a[i+1]=i/3+1;a[i+2]=i/3+2;}
+        else{a[i]=a[i-1];a[i+1]=a[i-2];a[i+2]=a[i-1]+1;}
+        i+=3;
+    }
+
     return a;
 };
 
@@ -340,10 +497,128 @@ CanvasGL.prototype._loadProgram = function()
     return program;
 };
 
+CanvasGL.prototype.setPixelPerfect = function(bool)
+{
+    this._pixelPerfect = bool;
+};
+
 CanvasGL.prototype.saveToPNG = function()
 {
     var canvas = window.open(this._glCanvas.toDataURL('image/png'));
 };
+
+//  0  1  2  3
+//  4  5  6  7
+//  8  9 10 11
+// 12 13 14 15
+
+function _makeMat44()
+{
+    return [ 1, 0, 0, 0,
+             0, 1, 0, 0,
+             0, 0, 1, 0,
+             0, 0, 0, 1 ];
+}
+
+function _makeMat44Translateion(x,y)
+{
+    return _mat44Translation(_makeMat44(),x,y);
+}
+
+function _makeMat44Scale(x,y)
+{
+    return _mat44Scale(_makeMat44(),x,y);
+}
+
+function _makeMat44Mult(m0,m1)
+{
+    return _mat44Mult(_makeMat44(),m0,m1);
+}
+
+function _mat44Mult(m,m0,m1)
+{
+    var d = m;
+
+    var a00 = m0[ 0], a01 = m0[ 1], a02 = m0[ 2], a03 = m0[3];
+    var a10 = m0[ 4], a11 = m0[ 5], a12 = m0[ 6], a13 = m0[7];
+    var a20 = m0[ 8], a21 = m0[ 9], a22 = m0[10], a23 = m0[11];
+    var a30 = m0[12], a31 = m0[13], a32 = m0[14], a33 = m0[15];
+
+    var b0  = m1[0], b1 = m1[1], b2 = m1[2], b3 = m1[3];
+    d[0] = b0*a00 + b1*a10 + b2*a20 + b3*a30;
+    d[1] = b0*a01 + b1*a11 + b2*a21 + b3*a31;
+    d[2] = b0*a02 + b1*a12 + b2*a22 + b3*a32;
+    d[3] = b0*a03 + b1*a13 + b2*a23 + b3*a33;
+
+    b0 = m1[4];
+    b1 = m1[5];
+    b2 = m1[6];
+    b3 = m1[7];
+    d[4] = b0*a00 + b1*a10 + b2*a20 + b3*a30;
+    d[5] = b0*a01 + b1*a11 + b2*a21 + b3*a31;
+    d[6] = b0*a02 + b1*a12 + b2*a22 + b3*a32;
+    d[7] = b0*a03 + b1*a13 + b2*a23 + b3*a33;
+
+    b0 = m1[8];
+    b1 = m1[9];
+    b2 = m1[10];
+    b3 = m1[11];
+    d[8] = b0*a00 + b1*a10 + b2*a20 + b3*a30;
+    d[9] = b0*a01 + b1*a11 + b2*a21 + b3*a31;
+    d[10] = b0*a02 + b1*a12 + b2*a22 + b3*a32;
+    d[11] = b0*a03 + b1*a13 + b2*a23 + b3*a33;
+
+    b0 = m1[12];
+    b1 = m1[13];
+    b2 = m1[14];
+    b3 = m1[15];
+    d[12] = b0*a00 + b1*a10 + b2*a20 + b3*a30;
+    d[13] = b0*a01 + b1*a11 + b2*a21 + b3*a31;
+    d[14] = b0*a02 + b1*a12 + b2*a22 + b3*a32;
+    d[15] = b0*a03 + b1*a13 + b2*a23 + b3*a33;
+
+    return d;
+}
+
+function _mat44Translation(m,x,y)
+{
+    m[12] = m[ 0] * x + m[ 4] * y + m[12];
+    m[13] = m[ 1] * x + m[ 5] * y + m[13];
+    m[14] = m[ 2] * x + m[ 6] * y + m[14];
+    m[15] = m[ 3] * x + m[ 7] * y + m[15];
+
+    return m;
+}
+
+
+function _mat44Scale(m,x,y)
+{
+    m[0] *= x;
+    m[1] *= x;
+    m[2] *= x;
+    m[3] *= x;
+    m[4] *= y;
+    m[5] *= y;
+    m[6] *= y;
+    m[7] *= y;
+    m[8]  = 0;
+    m[9]  = 0;
+    m[10] = 0;
+    m[11] = 0;
+
+    return m;
+}
+
+function _mat44Identity(m)
+{
+   m[ 0] = 1; m[ 1] = m[ 2] = m[ 3] = 0;
+   m[ 5] = 1; m[ 4] = m[ 6] = m[ 7] = 0;
+   m[10] = 1; m[ 8] = m[ 9] = m[11] = 0;
+   m[15] = 1; m[12] = m[13] = m[14] = 0;
+
+    return m;
+}
+
 
 
 function colori()
