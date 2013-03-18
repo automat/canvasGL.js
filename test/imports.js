8b2968c45acfc177d32baf69b10e9e08e3ae51ce
automat <mail@finge.cc>
2013-03-18 09:11:09 +0100

    refactoring...

--- test/imports.js
+++ test/imports.js
@@ -1,5 +1,5 @@
 document.write('<script type="text/javascript" src="js/webgl-debug.js""></script> ');
-document.write('<script type="text/javascript" src="js/canvasGL.js"></script>');
+document.write('<script type="text/javascript" src="../src/canvasGL.js"></script>');
 document.write('<script type="text/javascript" src="js/requestAnimationFrame.js"></script>');
 document.write('<script type="text/javascript" src="js/math.js"></script>');
 document.write('<script type="text/javascript" src="js/perlin-noise-simplex.js"></script>');
