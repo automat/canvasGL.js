3fa646b1c30a1f2f5ba59b3dda0427c1a9941a25
Henryk Wollik <olafyoorsen@hotmail.com>
2014-12-08 23:00:02 +0100

    polyline++

--- examples/01_Primitives/app.js
+++ examples/01_Primitives/app.js
@@ -13,65 +13,93 @@ App.prototype.constructor = App;
 
 App.prototype.draw = function(){
     var ctx = this.getContext();
-    ctx.background(1,0,0);
+    ctx.background(1,1,1);
 
     var s = this.secondsElapsed();
-    var intrpl = 0.5 + Math.sin(s) * 0.5;
 
-    var gl = ctx._gl;
-    var program = ctx._program;
 
+    ctx.strokeCap(CanvasGL.ROUND);
     ctx.pointSize(6.0);
 
-    //var buffer = gl.createBuffer();
-    //gl.bindBuffer(gl.ARRAY_BUFFER,buffer);
-    //gl.bufferData(gl.ARRAY_BUFFER,new Float32Array([0,0,1,1,1,1]),gl.STATIC_DRAW);
-    //ctx.setMatrixUniform();
-    //program.vertexAttribPointer('aVertexPosition',2,gl.FLOAT,false,0,0);
-    //program.vertexAttribPointer('aVertexColor',4,gl.FLOAT,false,0,8);
-    //program.disableVertexAttribArray('aTexcoord');
-    //gl.drawArrays(gl.POINTS,0,1);
-    //program.enableVertexAttribArray('aTexcoord');
-    //gl.deleteBuffer(buffer);
 
-    //ctx.pushMatrix();
-    //ctx.fill(1,1,1,1);
-    //ctx.translate(this.width() * 0.5,this.height() * 0.5);
-    //ctx.rotate(i * Math.PI * 2.0);
-    //ctx.point(100,0);
-    //ctx.popMatrix();
-
-    //ctx.pushMatrix();
-    //ctx.rotate(s);
-    //ctx.fill(1,1,1,1);
-    //ctx.points([100,0,0,0,100,100,50,50],[1,1,1,1,1,0,1,1]);
-    //ctx.popMatrix();
-
-    //ctx.pushMatrix();
-    //ctx.fill(1,1,1,1);
-    //ctx.circleDetail(10 + (intrpl * 10));
-    //ctx.circleMode(CanvasGL.CENTER);
-    //ctx.circle(100,100,100);
-    //ctx.circleMode(CanvasGL.CORNER);
-    //ctx.circle(100,100,100);
-    //ctx.popMatrix();
-
-    ctx.pushMatrix();
-    ctx.stroke(1,1,1,1);
-    ctx.strokeWeight(10);
-    ctx.translate(100,100);
-    ctx.strokeCap(CanvasGL.ROUND);
-   // ctx._polyline([0,0,100,0,100 + Math.sin(s) * 50,100,0,100],null,true);
-    //ctx.lines([[0,0,100,0,100,100],[100,150,200,150]],null,false);
-    ctx.strokeWeight(20);
-    ctx.line(100,100,0,100);
-    //ctx.line(0,200,50,180);
+    //ctx.stroke(1,1,1);
+    //ctx.strokeWeight(10);
+    //ctx.line(20,20,380,20);
+    //ctx.stroke(0.25,0,0);
+    //ctx.strokeWeight(5);
+    //ctx.line(380,20,380,380);
+    //ctx.stroke(0,0.25,0);
+    //ctx.strokeWeight(20);
+    //ctx.line(380,380,20,380);
+    //ctx.stroke(0,0,0.25);
+    //ctx.strokeWeight(15);
+    //ctx.line(20,380,20,20);
     //
     ctx.strokeWeight(2);
-    //ctx.lines([150,0,150,20,130,50,180,80]);
-    //ctx.line(0,0,100,0);
-    //ctx._polyline([0,0,100,0,100,100,0,100],null,false);
+    ctx.stroke(0,0,1);
+    ctx.lines([40,40,360,40,200,360],true);
+
+
+    var l = 500, l_1 = 1.0 / (l - 1), arr = new Array(l * 2);
+    var i = -1;
+    while(++i < l){
+        arr[i * 2]     = 40 + i * l_1 * 320;
+        arr[i * 2 + 1] = 200 + Math.sin(i*l_1 * Math.PI * 4 - s * 4) * 50;
+
+    }
+    ctx.strokeWeight(30);
+    ctx.stroke(0);
+    //ctx.lines(arr);
+
+    ctx.pushMatrix();
+    ctx.translate(0,-2);
+    ctx.stroke([1,0,1,1,0,0,1,1]);
+    //ctx.lines(arr);
     ctx.popMatrix();
+
+    ctx.pushMatrix();
+
+
+    ctx.translate(this.width(), 0);
+    ctx.rotate(Math.PI * 0.5);
+    ctx.strokeWeight(60);
+    ctx.lines(arr);
+    ctx.translate(-5,0);
+    ctx.stroke([0.25,0.15,0.15,1.0,0,0,0,1.0]);
+    ctx.lines(arr);
+    ctx.popMatrix();
+
+    ctx.pushMatrix();
+    ctx.translate(100,100);
+    ctx.scale(0.5,0.5);
+    ctx.stroke(1,0,1,1);
+    ctx.lines(arr);
+    ctx.translate(0,-10);
+    ctx.stroke([1,1,1,1,1,0.65,1,1]);
+    ctx.lines(arr);
+    ctx.popMatrix();
+
+    ctx.pushMatrix();
+    ctx.strokeWeight(1);
+    ctx.translate(0,30);
+    i = -1; l = 10; l_1 = 1.0 / (l - 1);
+    while(++i < l){
+        ctx.translate((this.width() - 40)/l,0);
+        ctx.strokeWeight(1 + i * 2);
+        ctx.stroke([i * l_1,0,i * l_1,1,1,0,0,1]);
+        ctx.line(0,0,0,20);
+    }
+    ctx.popMatrix();
+
+    i = -1; l = 500; l_1 = 1.0 / (l-1);
+    while(++i < l){
+        arr[i * 2    ] = i * l_1 * 200;
+        arr[i * 2 + 1] = Math.cos(i * l_1 * Math.PI / 2) * 100;
+    }
+    ctx.lines(arr);
+
+    //
+    //ctx.lines([30,200,370,200,370,370,30,370]);
 };
 
 window.addEventListener('load',function(){
